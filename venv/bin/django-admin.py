d5fdd21eddc80a3034f5696a0568e24872a2830d
Mandar Mulherkar <mandar@getmiles.com>
2020-02-09 16:57:34 -0800

    Update gitignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mandar/connectIQ/workspace_poc/css/cssprocessing/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
